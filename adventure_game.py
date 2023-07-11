7edaf747ee22893616d23d0ee81d52d4a175f990
JimmyIssacs <139177105+JimmyIssacs@users.noreply.github.com>
2023-07-11 14:45:21 -0400

    Update adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -1,40 +1,48 @@
 import random
+import enemy
+encounters = ["zombie", "skeleton", "witch", "bandit", "nothing", "cave", "ruins", "canyon", "river"]
 arrow = 0
 burger = 0
 name = input("What is your name?")
-money = 20.00
+money = 20
 stats = input("Pick a class: Fighter, Wizard, Archer, or Rogue")
 if stats == "Fighter":
     health = 100
+    max_health = 100
     magic = 0
     armor = 70
     attack = 70
+    damage = 10
     ranged = 30
     stealth = 20
 elif stats == "Wizard":
     health = 60
+    max_health = 60
     magic = 90
     armor = 45
     attack = 50
+    damage = 10
     ranged = 25
     stealth = 60
 elif stats == "Archer":
     health = 75
+    max_health = 75
     magic = 0
     armor = 85
     attack = 50
+    damage = 10
     ranged = 95
     stealth = 60
     arrow = 20
 elif stats == "Rogue":
     health = 60
+    max_health = 60
     magic = 0
     armor = 45
     attack = 80
+    damage = 10
     ranged = 25
     steath = 90
-
-    
 while True:
     if burger > 0:
         if input(f"Do you want to eat cheeseburger? You have {burger} cheeseburger(s) left") == "yes":
@@ -48,15 +56,42 @@ while True:
                     if money >= 5:
                         arrow += 10
                         money -= 5
+                        print("Jake: Here are your arrows.")
                     else:
                         print ("Jake: You do not have enough money.")
                     if (item == "cheeseburger" or item == "Cheeseburger"):
                         if money >= 5:
                             burger += 1
                             money -= 5
+                            print("Jake: Here is your cheseburger.")
                         else:
                             print("Jake: You do not have enough money.")
-                        
-    action = input("What do you want to do this turn? You can rest, explore, or build.")
+                    if (item == "blade sharpening" or item == "Blade Sharpening"):
+                        if money >= 50:
+                            attack += 10
+                            money -= 50
+                            ("Jake: Your blade has been sharpened.")
+                        else:
+                            print("Jake: You do not have enough money.")
+                    if (item == "Armor Reinforcement" or item == "armor reinforcement"):
+                        if money >= 70:
+                            armor += 10
+                            money -= 70
+                            print("Jake: Your armor has been reinforced.")
+                        else:
+                            print("Jake: You do not have enough money.")
+    action = input("What do you want to do this turn? You can rest or explore.")
     if (action == "rest" or action == "Rest"):
-        ''
+        if health + 20 <= max_health:
+            health += 20
+        else:
+            health = max_health
+        random.randint(0,100)
+        if random.randint <= 30:
+            money = money//2
+            print("You got robbed in your sleep, you lost half your money.")
+        else:
+            print("You sleep peacefully.")
+    elif (action == "explore" or action == "Explore"):
+        random.choice(encounters)
+        
